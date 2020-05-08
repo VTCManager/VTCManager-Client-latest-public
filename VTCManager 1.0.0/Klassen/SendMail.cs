05654f3207b8c217d6941a2154144b96171570c9
edvbl <edvbl@DESKTOP-GRPF9D4>
2020-05-08 04:02:14 +0200

    Add DLC_Log.txt to Email

--- VTCManager 1.0.0/Klassen/SendMail.cs
+++ VTCManager 1.0.0/Klassen/SendMail.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Mail;
 using System.Threading;
@@ -15,7 +17,12 @@ namespace VTCManager_1._0._0.Klassen
      
             File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\VTC_LOG_COPY.txt");
             File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\VTC_SYSTEM_LOG_COPY.txt");
-            
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\DLC_Log.txt");
+
+
+            Erstelle_DLC_LOG();
+
+
             MailMessage mail = new MailMessage();
             mail.From = new MailAddress("vtc_diag@web.de"); 
             mail.To.Add("edv.blasius@gmail.com"); 
@@ -28,8 +35,8 @@ namespace VTCManager_1._0._0.Klassen
 
             mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\VTC_LOG_COPY.txt"));
             mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\VTC_SYSTEM_LOG_COPY.txt"));
+            mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\DLC_Log.txt"));
             mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Euro Truck Simulator 2\game.log.txt"));
-            mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Euro Truck Simulator 2\mods_info.SII"));
 
             mail.IsBodyHtml = true;
             SmtpClient client = new SmtpClient("smtp.web.de", 587); 
@@ -52,6 +59,79 @@ namespace VTCManager_1._0._0.Klassen
             mail.Dispose();
         }
 
+
+        private void Erstelle_DLC_LOG()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + @"\VTC_Manager\DLC_Log.txt");
+
+            DirectoryInfo d = new DirectoryInfo(utils.Reg_Lesen("TruckersMP_Autorun", "ETS2_Pfad"));
+            FileInfo[] Files = d.GetFiles("dlc_*");
+            List<string> myCollection = new List<string>();
+            foreach (FileInfo file in Files)
+            {
+                myCollection.Add(file.Name + Environment.NewLine);
+            }
+            string a = String.Join("", myCollection);
+
+            File.WriteAllText(path, a);
+
+            DirectoryInfo d2 = new DirectoryInfo(utils.Reg_Lesen("TruckersMP_Autorun", "ETS2_Pfad") + @"\bin\win_x64");
+            FileInfo[] Files2 = d2.GetFiles("*.*");
+            List<string> myCollection2 = new List<string>();
+            foreach (FileInfo file2 in Files2)
+            {
+                myCollection2.Add(file2.Name + Environment.NewLine);
+            }
+            string a2 = String.Join("", myCollection2);
+            File.AppendAllText(path, " ------------------------------    GAME VERZEICHNIS   ------------------------------------" + Environment.NewLine);
+            File.AppendAllText(path, a2);
+
+
+            DirectoryInfo d3 = new DirectoryInfo(utils.Reg_Lesen("TruckersMP_Autorun", "ETS2_Pfad") + @"\bin\win_x64\plugins");
+            FileInfo[] Files3 = d3.GetFiles("*.*");
+            List<string> myCollection3 = new List<string>();
+            foreach (FileInfo file3 in Files3)
+            {
+                myCollection3.Add(file3.Name + Environment.NewLine);
+            }
+            string a3 = String.Join("", myCollection3);
+            File.AppendAllText(path, " ------------------------------    PLUGIN VERZEICHNIS   ------------------------------------" + Environment.NewLine);
+            File.AppendAllText(path, a3);
+
+            File.AppendAllText(path, " ------------------------------        REGISTRY         ------------------------------------" + Environment.NewLine);
+            File.AppendAllText(path, "ANTI-AFK: " + Read("ANTI_AFK") + Environment.NewLine +
+                "ANTI_AFK_AN: " + Read("ANTI_AFK_AN") + Environment.NewLine +
+                "Background: " + Read("Background") + Environment.NewLine +
+                "Diagnostic: " + Read("Diagnostic") + Environment.NewLine +
+                "ETS Pfad: " + Read("ETS2_Pfad") + Environment.NewLine +
+                "ATS Pfad: " + Read("ATS_Pfad") + Environment.NewLine +
+                "Plugins ETS: " + Read("Plugins ETS") + Environment.NewLine +
+                "Plugins ATS: " + Read("Plugins ATS") + Environment.NewLine +
+                "Reload Traffic Sek: " + Read("Reload_Traffic_Sekunden") + Environment.NewLine +
+                "Verkehr Server: " + Read("verkehr_SERVER") + Environment.NewLine +
+                "Version: " + Read("Version") + Environment.NewLine);
+        }
+        public string Read(string KeyName)
+        {
+            RegistryKey rk = Registry.CurrentUser;
+            RegistryKey sk1 = rk.OpenSubKey(@"Software\VTCManager\TruckersMP_Autorun");
+            if (sk1 == null)
+            {
+                return null;
+            }
+            else
+            {
+                try
+                {
+                    return (string)sk1.GetValue(KeyName.ToUpper());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Reading registry " + KeyName.ToUpper());
+                    return null;
+                }
+            }
+        }
 
     }
 }
