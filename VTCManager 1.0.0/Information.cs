df2db8a77b28ebf692f8ad235d846d9388c30a6e
Joschua H <joschua.hass@t-online.de>
2020-05-15 09:06:14 +0200

    global update

--- VTCManager 1.0.0/Information.cs
+++ VTCManager 1.0.0/Information.cs
@@ -9,7 +9,7 @@ namespace VTCManager_1._0._0
     static class Information
     {
         //Versions
-        public static string ClientVersion = "2.2.9";           // Eigene Revisionsnummer
+        public static string ClientVersion = "2.3.0";           // Eigene Revisionsnummer
         public static string TelemetryVersion = "1.10";         // Telemetry Version. 11 ist die letzte, die haben wir derzeit Testweise in Erprobung. 
         public static string SCSSdkVersion = "4.0.30319";       // Neue Versionsnummer -> Steht nei der SCSSdkClient.dll in Eigenschaften. Wenn kleiner bitte Updaten
         //Discord
